ff979464bf42b37de97656c7b4a7803b3dac8475
Jayakumar Mahesh <jm25acy@herts.ac.uk>
2025-11-28 01:14:40 +0000

    "Added mean perception of an season"

--- Jayakumar/Jayakumar-Analysis2.R
+++ Jayakumar/Jayakumar-Analysis2.R
@@ -1,5 +1,14 @@
 # R codes used for analysis and visualization 
 
 library(readr)
-df <- read_csv("london_weather.csv")
+weather_df <- read_csv("london_weather.csv")
 View(df)
+
+weather_df$year <- as.numeric(substr(weather_df$date, 1, 4))
+weather_df$month <- as.numeric(substr(weather_df$date, 5, 6))
+
+weather_df$season <- ifelse(weather_df$month %in% c(12, 1, 2), "Winter",
+                            ifelse(weather_df$month %in% c(3, 4, 5), "Spring",
+                                   ifelse(weather_df$month %in% c(6, 7, 8), "Summer", "Autumn")))
+
+weather_df <- weather_df[!is.na(weather_df$precipitation), ]
