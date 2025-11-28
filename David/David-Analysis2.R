0a0235e22fbc7f7d6b92c816a6b239fb3fcf17cb
do25ab <do25abe@herts.ac.uk>
2025-11-28 13:03:57 +0000

    R codes for data visualisation: this include codes for boxplot and histogram with distribution curve overlay

--- David/David-Analysis2.R
+++ David/David-Analysis2.R
@@ -1,5 +1,45 @@
-# R codes used for analysis and visualization 
-
 library(readr)
-df <- read_csv("london_weather.csv")
-View(df)
+df<- read_csv("london_weather.csv")
+df$year<- as.numeric(substr(as.character(df$date),1,4))
+df$month<- as.numeric(substr(as.character(df$date),5,6))
+df$season[df$month %in% c(3,4,5)]<- "Spring"
+df$season[df$month %in% c(6,7,8)]<- "Summer"
+df$season[df$month %in% c(9,10,11)]<- "Autumn"
+df$season[df$month %in% c(12,1,2)]<- "Winter"
+df$season <- factor(df$season,
+                    levels = c("Spring","Summer","Autumn","Winter"))
+
+png("Box_plot.png")  
+
+boxplot(df$precipitation ~ df$season, 
+        xlab = "seasons",
+        ylab = "Daily precipitation(mm)",
+        main = "The Boxplot Diagram")
+
+dev.off()
+
+
+
+#R script for Histogram
+
+
+y<-df$precipitation
+h<- hist(y,     #variable to count
+         40,    #number of bars
+         
+         main = "Histogram of daily precipitation(London weather)",  #Title of chart
+         
+         xlab = "Daily Precipitation(mm)",  #x-axis label
+         ylab = "Frequency",                #y-axis label
+         col = "skyblue"                   #fill color for the bars
+)
+
+#R script for Overlay
+mn<-mean(y) #mean of the data (precipitation)
+stdD<-sd(y) #standard deviation of the data
+x<-seq(0.000,61.800) #x-axis points
+y1<-dnorm(x,mean=mn,sd=stdD) # an idealized normal curve
+y1<-y1*diff(h$mids[1:2]) * length(y)  #calculate the curve against actual lines
+lines(x, y1,col ="black")  #plot calibrated line in black
+
+
